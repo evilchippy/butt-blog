c59af8f9d29872fc4af6b1aa67ac2c32746e1d0f
Michael Liberman <admin@stealasofa.com>
2020-03-11 16:26:55 -0700

    Finished Butt Blog

---
+++ README.md
@@ -0,0 +1,16 @@
+# Blog Project
+-   pkill -f runserver
+
+## Steps
+1.  Create models
+2.  Create forms
+3.  Add directories for templates and static files
+4.  Register directories in settings.py
+5.  Create views
+    - Require authentication
+    - SQL Queries
+6.  Create urls
+    - Link app URLs to project URLs
+7.  Create Templates
+    - Base template
+    - Page specific templates
---
+++ blog/models.py
@@ -0,0 +1,46 @@
+from django.db import models
+from django.utils import timezone
+from django.urls import reverse
+
+
+# Create your models here.
+class Post(models.Model):
+    # AUTHOR MUST BE A SUPERUSER
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
+
+    # SETS THE PUBLISHED_DATE TO THE MOMENT THE PUBLISH BUTTON IS CLICKED
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def approve_comments(self):
+        return self.comments.filter(approved_comment=True)
+
+    # RETURNS USER TO A SPECIFIED URL AFTER ACTION IS TAKEN
+    def get_absolute_url(self):
+        return reverse("post_detail", kwargs={'pk': self.pk})
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    post = models.ForeignKey('blog.Post', related_name='comments', on_delete=models.CASCADE)
+    author = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    approved_comment = models.BooleanField(default=False)
+
+    def approve(self):
+        self.approved_comment = True
+        self.save()
+
+    def get_absolute_url(self):
+        return reverse("post_list")
+
+    def __str__(self):
+        return self.text
